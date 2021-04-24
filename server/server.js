614aa005049877c126d7a406c22fad6aa8f63ece
akikoinhd <akikoinhd@gmail.com>
2021-04-24 14:33:22 -0500

    fetch requests to trefle API examples in server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const PORT = 3000;
 
 // Connection URLs:
-const TOKEN_QUERY = '?token=9dp4rcwCMuudpX55TiiVU0HDQCh3OmOcRJXePNUW2_w'
+const TOKEN = '9dp4rcwCMuudpX55TiiVU0HDQCh3OmOcRJXePNUW2_w'
+const TREFLE = 'https://trefle.io'
 const TREFLE_DIST = 'https://trefle.io/api/v1/distributions'
 
 
@@ -29,15 +30,76 @@ app.get('/', (req, res) => {
 // FETCHING DATA FROM TREFLE TO STRUCTURE QUERIES
 
 const fetch = require('node-fetch');
+const example = 'Oregon'
 
-(async () => {
-  const response = await fetch(`${TREFLE_DIST}${TOKEN_QUERY}&q=Colorado`);
+
+const fetchDistro = async (input) => {
+
+  const response = await fetch(`${TREFLE_DIST}?token=${TOKEN}&q=${input}`);
   const json = await response.json();
-  console.log(json);
-})();
+  // const url = `/api/v1/distributions/${json.data[0].slug}/species`;
+  const url = `${TREFLE_DIST}/${json.data[0].slug}/plants?filter%5Bestablishment%5D=native&token=${TOKEN}`
+  getPlants(url);
+};
 
+// "${TREFLE_DIST}/${json.data[0].slug}/plants?filter%5Bestablishment%5D=native&token=9dp4rcwCMuudpX55TiiVU0HDQCh3OmOcRJXePNUW2_w"
+//{ total: 2574 } { total: 665 }
+const getPlants = async (input) => {
+  // const response = await fetch(`${TREFLE}${url}${TOKEN_QUERY}`);
+  const response = await fetch(input);
+  const json = await response.json();
+  const url = json.data[1].links.self;
+  // const obj = {}
+  // console.log(json.meta);
+  // json.data.forEach(plant => {
+  //   obj[plant["family_common_name"]] = true;
+  // })
+
+  // console.log(Object.keys(obj))
+  console.log(json.meta.total)
+  console.log(json.data[10].common_name)
+  console.log(url)
+  getSpecies(url);
+}
+
+const getSpecies = async (input) => {
+  console.log(input)
+  console.log(`${TREFLE}${input}?token=${TOKEN}`);
+  const response = await fetch(`${TREFLE}${input}?token=${TOKEN}`);
+  const json = await response.json();
+  console.log(Object.keys(json.data));
+  console.log(json.data.observations)
+}
+
+
+
+fetchDistro(example);
+
+// /api/v1/distributions/col/plants
+// "/api/v1/plants/plantago-lanceolata"
+// '/api/v1/species/plantago-lanceolata'
 // ************************************************
 
+/**
+ * [
+  'id',            'common_name',
+  'slug',          'scientific_name',
+  'year',          'bibliography',
+  'author',        'status',
+  'rank',          'family_common_name',
+  'genus_id',      'observations',
+  'vegetable',     'image_url',
+  'genus',         'family',
+  'duration',      'edible_part',
+  'edible',        'images',
+  'common_names',  'distribution',
+  'distributions', 'flower',
+  'foliage',       'fruit_or_seed',
+  'sources',       'specifications',
+  'synonyms',      'growth',
+  'links'
+]
+ */
 
 
 // unknown path handler
