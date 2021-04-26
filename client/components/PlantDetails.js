08c4ad87b336f2351db9df3c511e22530a3b2d52
Jason Fricano <jasonfricano@Ninjas-MBP.local>
2021-04-25 22:11:26 -0400

    components rendering, ready for backend api

--- client/components/PlantDetails.js
+++ client/components/PlantDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import { getPlantDetails } from '../../helpers';
 import helpers from '../../helpers';
 // import Button from "@material-ui/core/Grid";
@@ -6,27 +6,44 @@ import helpers from '../../helpers';
 
 const PlantDetails = props => {
 
-  const { selectedPlant } = props;
+  const { family } = props;
+  const { plant } = props;
   const { plantDetails } = props;
   const { setPlantDetails } = props;
+  const { gState } = props;
 
-  // setPlantDetails(helpers.getPlantDetails(selectedPlant));
+  useEffect(
+    // () => {
+    //   fetch(`/${gState}/${family}/${plant}`)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       setPlantDetails(data);
+    //     })
+    //     .catch(() => console.log('oops'));
+    // },
+    () => {
+      const obj = {
+        detail1: 'green plant',
+        detail2: 'edible'
+      };
+      setPlantDetails(obj);
+      console.log('plantDeets', plantDetails);
+    }, 
+    [plant]
+  )
 
-  // this is wrong- have to render nothing at first
-  // and then only update every time the selectedPlant is reset
-  // and re-render every time the selectedPlant changes
-  // is this an effect????
-  if (selectedPlant === null) return (<div></div>)
+  if (plant === null) return (<div></div>)
+
   else return (
     <ul>
       <li>
-        {plantDetails.detail1};
+        {`detail1: ${plantDetails.detail1}`}
       </li>
       <li>
-        {plantDetails.detail2};
+        {`detail2: ${plantDetails.detail2}`}
       </li>
     </ul>
-  )
+  );
 }
 
 
