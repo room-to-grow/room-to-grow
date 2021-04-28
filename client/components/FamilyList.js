f4fb733c4c65bbf4c47cd4ca252b2c47a2ef8d11
mikeFeldman15 <michaelfeldma1@gmail.com>
2021-04-28 18:54:24 -0400

    frontend work

--- client/components/FamilyList.js
+++ client/components/FamilyList.js
@@ -2,6 +2,8 @@
 import React, {useEffect, useState} from 'react';
 //import { Button } from '@material-ui/core';
 // import helpers from '../../helpers';
+import PlantList from './PlantList';
+import PlantDetails from './PlantDetails';
 
 const FamilyList = props => {
   const [familiesData, setFamiliesData] = useState(null);
@@ -16,9 +18,13 @@ const FamilyList = props => {
   const { setGState } = props;
   const { family } = props;
   const { setFamily } = props;
+  const { plant } = props;
   const { setPlant } = props;
+  const { plantDetails } = props;
   const { setPlantDetails } = props;
-
+  const { favorites } = props;
+  const { loginName } = props;
+ 
   // families receives object
   // first k-v pair 
   // have to get the slug and pass back to backend with the family name
@@ -45,6 +51,7 @@ const FamilyList = props => {
   if(family === null) plants = <div></div>;
 
   else {
+    console.log('in the else')
     plants = (
   <div className='plantListComponent'>
   <PlantList
@@ -56,11 +63,34 @@ const FamilyList = props => {
   </div>)
   }
 
+  // let details;
+
+  // if(plant === null) details = <div></div>;
+
+  // else {
+  //   details = (
+  //     <div className='plantListComponent'>
+  //     <PlantDetails
+  //       family={family}
+  //       plant={plant}
+  //       gState={gState}
+  //       plantDetails={plantDetails}
+  //       setPlantDetails={setPlantDetails}
+  //       setFavorites={setFavorites}
+  //       favorites={favorites}
+  //       loginName={loginName}
+  //     />
+  //     </div>
+  //   )
+  // }
+
   if (familiesData === null) return (<div></div>)
   else return (
     <div className='popup-box'>
         <div className='box'>
           <span className='close-icon' onClick={() => {revertStates()}}>x</span>
+          
+          <div className='familyList-plantList-container'>
           <h1>See Plants In {gState.name}:</h1>
             <ul id="familyList">
               <div className='familyList-dropdown'>
@@ -90,6 +120,8 @@ const FamilyList = props => {
               </div>
             </ul>
             {plants}
+            {/* {details} */}
+            </div>
           </div>
         </div>
   );
